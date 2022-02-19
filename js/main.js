640330c253953d05e6bcd284bb60161a3e1e42d5
Shaun Leonar <81700189+ScriBrannauts@users.noreply.github.com>
2022-02-19 16:12:43 +0800

    Yes

--- js/main.js
+++ js/main.js
@@ -9,3 +9,32 @@ mobileBtn.addEventListener("click", () => {
 mobileBtnExit.addEventListener("click", () => {
     nav.classList.remove("menu-btn");
 });
+
+var button = document.getElementById('right-arrow');
+button.onclick = function () {
+    var container = document.getElementById('topics');
+    sideScroll(container,'right', 5, 100, 50);
+};
+
+var back = document.getElementById('left-arrow');
+back.onclick = function () {
+    var container = document.getElementById('topics');
+    sideScroll(container,'left', 5, 100, 50);
+};
+
+function sideScroll(element, direction, speed, distance, step){
+    scrollAmount = 0;
+
+    var slideTimer = setInterval(function(){
+        if(direction == 'left'){
+            element.scrollLeft -= step;
+        } else {
+            element.scrollLeft += step;
+        }
+
+        scrollAmount += step;
+        if(scrollAmount >= distance){
+            window.clearInterval(slideTimer);
+        }
+    }, speed);
+}
